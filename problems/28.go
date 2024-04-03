a8cfcf279d09ccf3187767130f8aee8aa74e735a
isseii10 <t1l.g2.o11g@gmail.com>
2024-04-03 23:35:02 +0900

    28

---
+++ problems/28.go
@@ -0,0 +1,19 @@
+package problems
+
+import "strings"
+
+func strStr(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+}
+
+func strStr2(haystack string, needle string) int {
+	n := len(needle)
+	res := -1
+	for i := 0; i+n <= len(haystack); i++ {
+		if haystack[i:i+n] == needle {
+			res = i
+			break
+		}
+	}
+	return res
+}
