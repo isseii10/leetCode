294fc62d7e3a16e303c15076dca04fa59a78b97c
isseii10 <t1l.g2.o11g@gmail.com>
2024-04-03 21:52:32 +0900

    58

---
+++ problems/58.go
@@ -0,0 +1,18 @@
+package problems
+
+func lengthOfLastWord(s string) int {
+	count := 0
+	counting := false
+	for i := len(s) - 1; i >= 0; i-- {
+		if counting && string(s[i]) == " " {
+			break
+		}
+		if string(s[i]) != " " {
+			if !counting {
+				counting = true
+			}
+			count++
+		}
+	}
+	return count
+}
---
+++ problems/58_test.go
@@ -0,0 +1,32 @@
+package problems
+
+import "testing"
+
+func Test_lengthOfLastWord(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{s: "Hello World"},
+			want: 5,
+		},
+		{
+			name: "case 2",
+			args: args{s: "   fly me   to   the moon  "},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLastWord(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLastWord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
