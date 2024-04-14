0b787701ebd31d3235baaec99d62ac3f169ad961
isseii10 <t1l.g2.o11g@gmail.com>
2024-04-14 14:57:42 +0900

    101 103 226

---
+++ top_interview_150/binary_tree_general/101.go
@@ -0,0 +1,27 @@
+package binarytreegeneral
+
+// 103,226を使った回答
+func isSymmetric(root *TreeNode) bool {
+	leftInv, right := invertTree(root.Left), root.Right
+	return isSameTree(leftInv, right)
+}
+
+func isSymmetric2(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return checkSymmetric(root.Left, root.Right)
+}
+
+func checkSymmetric(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left != nil && right != nil {
+		if left.Val == right.Val {
+			return checkSymmetric(left.Left, right.Right) && checkSymmetric(left.Right, right.Left)
+		}
+	}
+	return false
+}
